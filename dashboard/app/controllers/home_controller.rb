e79c09ee7c162c92c28dea562b5ff1cffe6fc033
sette17 <lacsonrosette@gmail.com>
2023-06-30 15:45:30 +0800

    added daily graphs

--- dashboard/app/controllers/home_controller.rb
+++ dashboard/app/controllers/home_controller.rb
@@ -4,4 +4,22 @@ class HomeController < ApplicationController
 
   def dashboard
   end
+
+  def daily
+    symbol = params[:symbol] # Get the selected symbol from the filter
+  
+    if symbol.present? # If a symbol is selected, filter the data accordingly
+      @chart_data = StockPricesDaily.where(symbol: symbol)
+                    .group("to_char(timestamp, 'YYYY-MM')")
+                    .sum(:close)
+    else # If no symbol is selected, fetch all data
+      @chart_data = StockPricesDaily.group("to_char(timestamp, 'YYYY-MM')")
+                    .sum(:close)
+    end
+  
+    render 'daily'
+  end
+  
+  
+  
 end
--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/dashboard'
+  get '/daily', to: 'home#daily', as: 'daily'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
