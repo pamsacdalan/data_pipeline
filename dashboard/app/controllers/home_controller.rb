820bbcf1c805ae4905b078f56cb773f72f03b345
Pamela Sacdalan <pamela.sacdalan@gmail.com>
2023-07-06 11:29:10 +0800

    added charts for intraday & weekly

--- dashboard/app/controllers/home_controller.rb
+++ dashboard/app/controllers/home_controller.rb
@@ -2,8 +2,33 @@ class HomeController < ApplicationController
   def index
   end
 
-  def dashboard
-  end
+  def intraday
+    @stock_prices = StockPricesIntraday.where("(symbol, timestamp) IN (
+      SELECT symbol, MAX(timestamp)
+      FROM stock_prices_intraday
+      GROUP BY symbol
+    )").all
+
+    @intra_stock_prices = StockPricesIntraday.all
+    @companies = StockPricesIntraday.distinct.pluck(:symbol) # get all companies
+
+    
+    # Get the selected symbol from the company filter
+    company = params[:company_intraday]
+    
+
+    if company.present?
+      @filtered_symbols = @companies & @intra_stock_prices.where(symbol: company).pluck(:symbol)
+    else
+      @filtered_symbols = @companies
+    end
+    
+    @data = @filtered_symbols.map do |filtered_symbol|
+        { name: filtered_symbol, data: @intra_stock_prices.where(symbol: filtered_symbol).pluck(:timestamp, :close).to_h }
+    end
+
+  end 
+      
 
   def daily
     symbol = params[:symbol] # Get the selected symbol from the filter
@@ -22,36 +47,49 @@ class HomeController < ApplicationController
   
 
   def weekly
-    # Default: All companies
+    # Get the data for the table
     @stock_prices = StockPricesWeekly.where("(symbol, timestamp) IN (
       SELECT symbol, MAX(timestamp)
       FROM stock_prices_weekly
       GROUP BY symbol
-    )").select(:symbol, :close, :percent_change, :average_price, :year_month)
+    )").all
 
-    @charts = StockPricesWeekly.select(:symbol, :average_price, :year_month)
+    # Get the selected symbol from the company filter
+    company = params[:company_weekly]
 
-    local  =  ['AAA', 'SM', 'TEL', 'GLO', 'UBP']
-    intl = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA']
+    # Data for the area-chart
+    @weekly_stock_prices = StockPricesWeekly.all
+    @companies = StockPricesWeekly.distinct.pluck(:symbol) # get all companies
 
-    # Region Filter
-    if params[:filter].present?
-      case params[:filter]
-      when 'local'
-        @stock_prices = @stock_prices.where(symbol: local)
-        @charts = @charts.where(symbol: local)
+    
 
-      when 'international'
-        @stock_prices = @stock_prices.where(symbol: intl)
-        @charts = @charts.where(symbol: intl)
-      end
+    # Get the ave %chg per symbol throught the year
+    @ave_chg_per_year = StockPricesWeekly.select('symbol, AVG(percent_change) AS ave_change, "timestamp"')
+                          .where("EXTRACT(YEAR FROM timestamp) = ?", 2023)
+                          .group('symbol, "timestamp"')
+                          .where(symbol: company)
+    
+    
+    if company.present?
+      @filtered_symbols = @companies & @weekly_stock_prices.where(symbol: company).pluck(:symbol)
+    else
+      @filtered_symbols = @companies
+      @ave_chg_per_year = StockPricesWeekly.select('symbol, AVG(percent_change) AS ave_change, "timestamp"')
+                                      .where("EXTRACT(YEAR FROM timestamp) = ?", 2023)
+                                      .group('symbol, "timestamp"')
+    end
+    
+    # Data for column chart
+    @data = @filtered_symbols.map do |filtered_symbol|
+      { name: filtered_symbol, data: @weekly_stock_prices.where(symbol: filtered_symbol).pluck(:year_month, :close).to_h }
+    end
+    
+    # Data for the line chart
+    @line_chart_data = @ave_chg_per_year.group_by(&:symbol).transform_values do |rows|
+      rows.map { |row| [row.timestamp.to_date, row.ave_change] }
     end
 
+    @series_data = @line_chart_data.map { |symbol, values| { name: symbol, data: values } }
 
- 
-
-
-  
   end
-
 end
--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/dashboard'
+  get '/intraday', to: 'home#intraday', as: 'intraday'
   get '/daily', to: 'home#daily', as: 'daily'
   get '/weekly', to: 'home#weekly', as: 'weekly'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "home#index"
+  root "home#intraday"
 end
